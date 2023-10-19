11b7c379bbf17be646ceda974d6ff6e8cce0c517
Sougata-Singha <117795609+Sougata-Singha@users.noreply.github.com>
2023-10-19 11:34:19 +0530

    create a test api route

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import mongoose from 'mongoose'
 import dotenv from 'dotenv'
+import userRouter from './routes/userRoutes.js'
 
 dotenv.config()
 
@@ -10,6 +11,9 @@ mongoose.connect(process.env.MONGO).then(() => {
     console.log(err)
 })
 const app = express()
+
+//user routes
+app.use('/api/user',userRouter)
 
 app.listen(3000, () => {
     console.log("Listening to http://localhost:3000")
