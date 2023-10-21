07947cea5e4da15f277cc67f5b319638aa7bad37
Sougata-Singha <117795609+Sougata-Singha@users.noreply.github.com>
2023-10-21 09:42:38 +0530

    created signup api

---
+++ api/controllers/authController.js
@@ -0,0 +1,16 @@
+import User from "../models/User.js"
+import bcrypt from 'bcryptjs'
+
+const singup = async (req,res) => {
+    const {username, email, password} = req.body
+    const hashedPassword = bcrypt.hashSync(password,10)
+    const newUser = new User({username, email, password : hashedPassword})
+
+    try {
+        await newUser.save()
+        res.status(201).json('User created successfully!')
+    } catch (err) {
+        res.status(500).json(err.message)
+    }
+}
+export default singup
--- api/index.js
+++ api/index.js
@@ -2,6 +2,7 @@ import express from 'express'
 import mongoose from 'mongoose'
 import dotenv from 'dotenv'
 import userRouter from './routes/userRoutes.js'
+import authRouter from './routes/auth.js'
 
 dotenv.config()
 
@@ -11,9 +12,12 @@ mongoose.connect(process.env.MONGO).then(() => {
     console.log(err)
 })
 const app = express()
+app.use(express.json())
 
 //user routes
 app.use('/api/user',userRouter)
+//authenticate route
+app.use('/api/auth',authRouter)
 
 app.listen(3000, () => {
     console.log("Listening to http://localhost:3000")
