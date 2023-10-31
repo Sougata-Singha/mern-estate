c5ea3007c48cb403bd4c0340447bd4ee60f427a1
Sougata-Singha <117795609+Sougata-Singha@users.noreply.github.com>
2023-10-31 15:30:53 +0530

    updated header and created protected routes

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import { FaSearch } from 'react-icons/fa'
+import {useSelector} from 'react-redux'
 
 const Header = () => {
+    const {currentUser} = useSelector((state) => state.user)
   return (
     <header className='bg-slate-200 shadow-md py-3 px-5'>
         <div className='flex justify-between items-center max-w-6xl m-auto'>
@@ -19,7 +21,10 @@ const Header = () => {
             <ul className='flex gap-3 text-slate-600 font-semibold'>
                 <Link to='/'><li className='hidden md:inline hover:underline'>Home</li></Link>
                 <Link to='/about'><li className='hidden md:inline hover:underline'>About</li></Link>
-                <Link to='sign-in'><li className='hover:underline'>Sign In</li></Link>
+                {currentUser ?
+                    <Link to='/profile'><img src={currentUser.avatar} className='rounded-full w-7 h-7 object-cover' alt="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png" /></Link> : 
+                    <Link to='sign-in'><li className='hover:underline'>Sign In</li></Link>
+                }
             </ul>
         </div>
     </header>
